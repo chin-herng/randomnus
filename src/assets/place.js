7b5332f7dbe80aa0b5120660808ec5cb3c1a8fb8
JustWeiHao <e0960531@u.nus.edu>
2024-01-20 21:45:42 +0800

    Add places, Align item

---
+++ src/assets/place.js
@@ -0,0 +1,22 @@
+const PLACES = [
+    'Udon Don Bar',
+    "Hwang's",
+    'Sapore',
+    'The Royals Bistro',
+    'Subway',
+    'Waa Cow!',
+    'Flavours',
+    'Fine Food',
+    'Starbucks',
+    'Mr. Bean',
+    'DoQoo',
+    'Super Snacks',
+    'InstaChef',
+    'Techno Edge',
+    'The Deck',
+    'Frontier',
+    'The Terrace',
+    'PGPR'
+];
+
+export default PLACES;
